047b1099d64d3cf89c4fe4cd9dd013b18e53ed28
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-09-14 20:57:55 +0100

    add db relationship

--- lopa/models.py
+++ lopa/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    description = models.TextField(blank=True, null=True)
+    target_frequency = models.FloatField(blank=True, null=True)
+
+
+class Cause(models.Model):
+    description = models.TextField(blank=True, null=True)
+    initial_frequency = models.FloatField(blank=True, null=True)
+    event_id = models.IntegerField(blank=True, null=True)
+
+
+class CauseBarrier(models.Model):
+    description = models.TextField(blank=True, null=True)
+    pfd = models.FloatField(blank=True, null=True)
+    cause_id = models.IntegerField(blank=True, null=True)
+
+
+class Consequence(models.Model):
+    description = models.TextField(blank=True, null=True)
+    target_frequency = models.FloatField(blank=True, null=True)
+    event_id = models.IntegerField(blank=True, null=True)
+
+
+class ConsequenceBarrier(models.Model):
+    description = models.TextField(blank=True, null=True)
+    pfd = models.FloatField(blank=True, null=True)
+    consequence_id = models.IntegerField(blank=True, null=True)
