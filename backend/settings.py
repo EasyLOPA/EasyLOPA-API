4aa75a161070b3ff15e6410b5f67d01d2721ad05
Seunfunmi Adegoke <51419148+seun-beta@users.noreply.github.com>
2021-09-17 20:09:08 +0100

    add lopa to installed apps

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wt*eep8u9o86=ju7vu2z(rxjub2wbp--_6tv6js%!que4%(^22
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'lopa.LopaConfig'
 ]
 
 MIDDLEWARE = [
